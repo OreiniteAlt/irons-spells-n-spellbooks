fef384dc2c01d47ba46deee91602cba42cfe687b
iron431 <34083081+iron431@users.noreply.github.com>
2025-03-24 09:53:40 -0500

    swap ender particle sprite, make ender projectile particles denser

--- src/main/java/io/redspace/ironsspellbooks/particle/UnstableEnderParticle.java
+++ src/main/java/io/redspace/ironsspellbooks/particle/UnstableEnderParticle.java
@@ -19,7 +19,7 @@ public class UnstableEnderParticle extends TextureSheetParticle {
         this.xd = xd;
         this.yd = yd;
         this.zd = zd;
-        this.quadSize = 0.1F * (this.random.nextFloat() * 0.15F + 0.3F);
+        this.quadSize = 0.2F * (this.random.nextFloat() * 0.15F + 0.3F);
         this.scale(2.25f);
         this.lifetime = 5 + (int) (Math.random() * 25);
         sprites = spriteSet;
