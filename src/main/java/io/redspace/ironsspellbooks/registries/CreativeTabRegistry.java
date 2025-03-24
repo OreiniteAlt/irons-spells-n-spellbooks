bfb3d39ba5edfcb47af9e0b2ba606f076fae57bd
iron431 <34083081+iron431@users.noreply.github.com>
2025-03-24 18:02:40 -0500

    bloodly vellum item, cursed doll feature complete

--- src/main/java/io/redspace/ironsspellbooks/registries/CreativeTabRegistry.java
+++ src/main/java/io/redspace/ironsspellbooks/registries/CreativeTabRegistry.java
@@ -166,10 +166,11 @@ public class CreativeTabRegistry {
                 entries.accept(ItemRegistry.BLOOD_VIAL.get());
                 entries.accept(ItemRegistry.DIVINE_PEARL.get());
 
+                entries.accept(ItemRegistry.MAGIC_CLOTH.get());
                 entries.accept(ItemRegistry.HOGSKIN.get());
+                entries.accept(ItemRegistry.BLOODY_VELLUM.get());
                 entries.accept(ItemRegistry.DRAGONSKIN.get());
                 entries.accept(ItemRegistry.ARCANE_ESSENCE.get());
-                entries.accept(ItemRegistry.MAGIC_CLOTH.get());
                 entries.accept(ItemRegistry.RUINED_BOOK.get());
                 entries.accept(ItemRegistry.CHAINED_BOOK.get());
                 entries.accept(ItemRegistry.CINDER_ESSENCE.get());
--- src/main/java/io/redspace/ironsspellbooks/registries/ItemRegistry.java
+++ src/main/java/io/redspace/ironsspellbooks/registries/ItemRegistry.java
@@ -173,6 +173,7 @@ public class ItemRegistry {
     public static final DeferredHolder<Item, Item> ICE_CRYSTAL = ITEMS.register("permafrost_shard", () -> new Item(ItemPropertiesHelper.material().rarity(Rarity.RARE)));
     public static final DeferredHolder<Item, Item> ENERGIZED_CORE = ITEMS.register("energized_core", () -> new EnergizedCoreItem(ItemPropertiesHelper.material(1).rarity(Rarity.UNCOMMON)));
     public static final DeferredHolder<Item, Item> CHAINED_BOOK = ITEMS.register("chained_book", () -> new Item(ItemPropertiesHelper.material()));
+    public static final DeferredHolder<Item, Item> BLOODY_VELLUM = ITEMS.register("bloody_vellum", () -> new Item(ItemPropertiesHelper.material()));
 
     public static final DeferredHolder<Item, Item> TIMELESS_SLURRY = ITEMS.register("timeless_slurry", () -> new Item(ItemPropertiesHelper.material().rarity(Rarity.UNCOMMON)));
     public static final DeferredHolder<Item, Item> PYRIUM_INGOT = ITEMS.register("pyrium_ingot", () -> new Item(ItemPropertiesHelper.material().rarity(CinderousRarity.CINDEROUS_RARITY_PROXY.getValue())));
